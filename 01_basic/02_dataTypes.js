abfad0d7b719a6833c7a4856d45edf6595556dff
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-11 17:40:06 +0000

    datatype

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"// treat all js code as a newer version
+ // alert(3+3) // we are use nodejs not a browers
+ console.log(3+
+    3
+ );
+ console.log("tahreem");
+ 
+ 
+ //number=> 2 to power53
+ //bigint
+ //string=>""
+ //boolean=>true/flase
+ //null=>stadardlone value
+ //undefined=>do'nt asign the value
+ //symbol=>unique
+
+ //object
+ console.log(typeof undefined);//undefined
+ console.log(typeof null);//object
