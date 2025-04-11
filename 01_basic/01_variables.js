716c893d8c7bd0923e427453240737c98092f635
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-11 10:02:33 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail= "tahreemtajalli@gmail.com"
+var accountPassword="12345"
+accountCity="Lahore"
+
+accountEmail="tahreem@gmail.com"
+accountPassword="123"
+accountCity="karachi"
+
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity])
