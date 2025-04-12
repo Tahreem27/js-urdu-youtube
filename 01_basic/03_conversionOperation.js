dc6665df404b967bb2ee10ad3bb78017ba35e158
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-12 08:08:34 +0000

    conversionOpertion

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score="33abc"
+
+//console.log(typeof score);//two option both are correct
+//console.log(typeof (score));
+
+let valueINnumber= Number(score)
+//console.log(typeof valueINnumber);
+//console.log(valueINnumber)
+// 33=>33
+//33abc=>Nan
+//true=>1
+//flase=>0
+//null=>0
+//tahreem=>Nan
+//undefined=>Nan
+
+
+let isLoggedIn="1"
+let boolenisLoggedIn= Boolean(isLoggedIn)
+//console.log( boolenisLoggedIn)
+//1=>true
+//0=>flase
+//tahreem=>true
+//""=>flase
+
+let SomeNumber= 33
+let StringNumber=String(SomeNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber)
