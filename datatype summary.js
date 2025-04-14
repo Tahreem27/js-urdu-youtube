206b206796d151bddfaf3a296283c8af01023232
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-14 08:00:17 +0000

    datatype summary.js

---
+++ datatype summary.js
@@ -0,0 +1,21 @@
+//Permitive(call by value)
+//7types: string,number,boolen,null,undefined,symbol,BigInt(sign,big value)
+
+const score=100;
+const scorevalue=30.2;
+var isLoggedvalue= false;
+let outsideTemp=null;
+let useremail;
+const id= Symbol("123")
+const bignumber= 1234567n
+
+
+
+//Reference(Non-Permitive)
+
+//array,object,function
+const hero[ali,ahmad]
+{
+    name:"tahreem";
+    age:22;
+}
