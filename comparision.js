263ae53ff0a0bb5b6f7342bacb108195483fc682
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-14 07:16:32 +0000

    comparision.js

--- comparision.js
+++ comparision.js
@@ -1,1 +1,19 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2==1);
+//console.log(2!=1);
+//avoid this all next comparision
+//console.log("2">1);
+//console.log("02">1);
 
+//console.log(null>0);//flase
+//console.log(null==0);//flase
+//console.log(null>=0);//true comparsion convert null to  number treating it as 0 that why null>=0mis true
+// and null>0 is flase.
+console.log(undefined<0);//flase
+console.log(undefined>0);//flase
+console.log(undefined>=0);//flase
+
+//strike check
+//===//value strikly check
+console.log("2"===2);//not a same that why it is flase
