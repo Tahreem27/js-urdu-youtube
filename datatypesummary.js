e201ac80c21a1ee5ee0b70ec1748b3ebb2ad1885
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-14 10:09:19 +0000

    datatypesummary.js

--- datatypesummary.js
+++ datatypesummary.js
@@ -1,23 +1,31 @@
 //Permitive(call by value)
 //7types: string,number,boolen,null,undefined,symbol,BigInt(sign,big value)
 
-const score=100;
-const scorevalue=30.2;
-var isLoggedvalue= false;
-let outsideTemp=null;
-let useremail;
-const id= Symbol("123")
-const bignumber= 1234567n
+//const score=100;
+//const scorevalue=30.2;
+//var isLoggedvalue= false;
+//let outsideTemp=null;
+//let useremail;
+//const id= Symbol("123")
+//const bignumber= 1234567n
 
 
-console.log(typeof isLoggedvalue)
+//odeconsole.log(typeof isLoggedvalue)
 //Reference(Non-Permitive)
 
 //array,object,function
 //const hero=[ali,ahmad]
-{
-    name:"tahreem";
-    age:22;
-}
-const MyFunction=function(){
-    console.log("hello world");}
+//{
+   // name:"tahreem";
+    //age:22;
+//}
+//const MyFunction=function(){
+  //  console.log("hello world");}
+    //+++++++++++++++++++++++++++++++++++++++++++++
+    //stack(permitive),Heap(non-permitive)
+    let myyoutubename="tahreemtajalli"
+    let anothername="myyoutubename"
+    anothername="chaiorcode"
+    console.log("myyoutubename");
+    console.log("anothername");
+    
