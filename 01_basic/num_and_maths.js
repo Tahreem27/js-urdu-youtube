8ecfbdf90cb9e7d839c81d09ef1e7d61eb5145fa
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-15 11:16:27 +0000

    num_and_maths.js

--- 01_basic/num_and_maths.js
+++ 01_basic/num_and_maths.js
@@ -1,2 +1,44 @@
-const newName= "tahreem"
-console.log(typeof newName)
+//const newName= "tahreem"
+//console.log(typeof newName)
+ 
+//const num=123
+//console.log(typeof num)
+
+//let number="1"
+//let boolennumber= Boolean(number)
+//console.log( boolennumber)
+//convert string to number
+//const str="123"
+//const num1= Number(str);
+//const result= num1+10;
+//console.log(result);
+
+
+//const num=123
+//const str= String(num)
+//console.log(str);
+
+const str2= "5"
+const num2= 5
+//console.log(str2==num2);//true
+console.log(str2===num2);//flase
+    //Perimitive//
+    let myYoutubename="tahreemtajalli"
+    let anotherName=myYoutubename
+  anotherName="chaiaurcode"
+    console.log(anotherName);
+    console.log(myYoutubename);
+    //non-perimitive
+    let userone={
+        email:"tahreemtajlli@gmail.com"
+    }
+    let usertwo= userone
+    usertwo.email="tahreem rana"
+    console.log(userone.email)
+    console.log(usertwo.email)
+    //summary of datatype
+   // primitive 
+   // string ,Boolean,Number,null 
+   // non-primitive data types.
+    //array,object,function.
+    
