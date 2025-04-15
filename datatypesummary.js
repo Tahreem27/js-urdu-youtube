7d52207e79edb4cf0ebfc6abb329e56ba309715c
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-15 09:42:35 +0000

    datatypesummary.js

--- datatypesummary.js
+++ datatypesummary.js
@@ -39,3 +39,22 @@
     const fristname="tahreem"
     const repoCount=52
     console.log(`MY NAME IS ${fristname} and repoCount is ${repoCount}`);
+
+    let gameName= new String("tahreem")
+    console.log(gameName[0]);
+    console.log(gameName.__proto__);
+    console.log(gameName.length);
+    console.log(gameName.toUpperCase());
+    console.log(gameName.charAt(2));
+    console.log(gameName .indexof ("t"));
+     const newString = gameName.substring(0,4);
+     console.log(newString);
+      const anotherString= gameName.slice(-8,4)
+      console.log(anotherString);
+     const newname= " tajalli "
+     console.log(newname.trim());
+       
+     const url="  https://tajalli.com//tajalli%20tahreem "
+     console.log(url.replace('%20','-'));
+     const newWord ="ball"
+     console.log(newWord.split('__'));
