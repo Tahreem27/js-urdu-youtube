e5100260e85833766901bce06734a90270021141
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-16 16:34:38 +0000

    dates.js

---
+++ 01_basic/dates.js
@@ -0,0 +1,10 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+let createDate= new Date(01-04-2025)
+console.log(createDate.toLocaleString());
+let myTimeStamp=Date.now()
+
+//console.log(myTimeStamp);
+//console.log(myCreateDate.getTime());
+console.log(Math.floor(Date.now()/1000));
