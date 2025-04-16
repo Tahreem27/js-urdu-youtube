36d4cff2e29b08228762cebd5afd266569bbc110
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-16 16:46:01 +0000

    dates.js

--- 01_basic/dates.js
+++ 01_basic/dates.js
@@ -8,3 +8,12 @@ let myTimeStamp=Date.now()
 //console.log(myTimeStamp);
 //console.log(myCreateDate.getTime());
 console.log(Math.floor(Date.now()/1000));
+
+let newDate= NewDate();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+weekday:"long",
+})
