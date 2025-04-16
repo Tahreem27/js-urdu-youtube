1d9e25298dcf5f430fe867c6ab06cb041e7f31f4
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-16 09:58:48 +0000

    maths.js

--- 01_basic/maths.js
+++ 01_basic/maths.js
@@ -7,4 +7,17 @@
  const anothernumber=123.456
 
  console.log(anothernumber.toPrecision(4));
+ //++++++++++Maths++++++++++++++++++++//
+ //console.log(Maths);
+ //console.log(Maths.abs(-4));
+ //console.log(Math.round(4.6));
+ //console.log(Maths.ceil(4.2));
+ //console.log(Math.min(4,5,3,9));
+ //console.log(Math.max(4,8,10,7));
+  //console.log((Math.random()*10)+1);
+  //console.log((Math .random()*10)+1)
+  const max= 20;
+  const min= 10;
+  console.log(Math.floor(Math.random()*(max-min+1))+1)
+
  
