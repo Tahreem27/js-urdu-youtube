b0809f485dc5435cc05074cba8d595f9f8f3b59b
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-16 06:25:59 +0000

    maths.js

---
+++ 01_basic/maths.js
@@ -0,0 +1,10 @@
+ const balance = new Number(100);
+ console.log(balance);
+
+ console.log(balance.toString().length);
+ console.log(balance.toFixed(1));
+
+ const anothernumber=123.456
+
+ console.log(anothernumber.toPrecision(4));
+ 
