08b03f9b6e23a60683b633b3a1752afa484fca7c
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-18 08:46:36 +0000

    array.js

--- 02_basic/array.js
+++ 02_basic/array.js
@@ -32,6 +32,23 @@
 
   const marvel_heros=["thor", "Ironman", "spiderman"]
   const hero_dc=["superman","flash","badman"]
-  console.log.push(hero_dc)
-  console.log.pop(marvel_heros)
-  console.log(marvel_heros [3][1])
+  //console.log.push(hero_dc)
+  //console.log.pop(marvel_heros)
+  //console.log(marvel_heros [3][1])
+ //spread operate
+  const all_heros=marvel_heros.concat(dc_heros)
+  //onsole.log(all_heros);
+  const all_new_heros=[...marvel_heros,...dc_heros]
+  //console.log(all_new_heros)
+  const another_array=[1,2,3[4,5,6[7,8]]]
+  const real_another_array= another_array.flat(Infinity)
+  console.log(real_another_array)
+  //form
+  console.log(Array.isArray("tahreem"))//boolen
+  console.log(Array.from("tahreem"))//array
+  console.log(Array.from({name:"tahreem"}))//interesting case output:[](empty array we can inform to keyarray and value than output is not empty)
+  
+  let score1=100
+  let score2=200
+  let score3=300
+  console.log(Arrayof(score1,score2,score3));
