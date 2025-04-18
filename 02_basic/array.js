2969c001629d7ca18c94a7f979c2c052d24e2207
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-18 07:21:20 +0000

    array.js

--- 02_basic/array.js
+++ 02_basic/array.js
@@ -1,19 +1,37 @@
 const myarry=[1,2,3,4,"tahreem"]
-console.log(myarry[0])
+//console.log(myarry[0])
 
 const Myarry = newarry(1,2,3,4);
-console.log(Myarry[1])
+//console.log(Myarry[1])
 //////method//////
 Myarry. push(6)
 Myarry.pop(7)
-console.log(Myarry);
+//console.log(Myarry);
 Myarry.unshift(9)
 Myarry .shift()
-console.log(Myarry);
-console.log(myarry.includes(9));
-console.log(myarry.indexOf(9));
+//console.log(Myarry);
+//console.log(myarry.includes(9));
+//console.log(myarry.indexOf(9));
   const newarry = Myarry.join;
-  console.log(Myarry);
-  console.log(typeof newarry);
+ // console.log(Myarry);
+  //console.log(typeof newarry);
+
+  //splice/slice
+ // console.log("A",myArr);
   
+  c//onst myn1 =myArr.slice(1,3)
+
+   // console.log(myn1);
+  //console.log("B",myArr);
+  
+  
+  //const myn2 =myArr.splice(1,3)
+  //console.log(myn2);
+ // console.log("c",myArr);
+
+
+  const marvel_heros=["thor", "Ironman", "spiderman"]
+  const hero_dc=["superman","flash","badman"]
+  console.log.push(hero_dc)
+  console.log.pop(marvel_heros)
   
