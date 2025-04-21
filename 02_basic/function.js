3f15153483bb997064e54e633b05a0b203e6e706
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-21 09:44:55 +0000

    function.js

---
+++ 02_basic/function.js
@@ -0,0 +1,16 @@
+function addtwonumbers(number1,number2){
+    console.log(number1+number2)
+}
+//addtwonumbers(3,4)
+function addtwonumbers(number1,number2){
+    let resulst=number1+number2
+     return resulst;
+}
+ //const resulst=addtwonumbers(3,4)
+ function loginusermessage(username){
+    if(username) {
+        console.log("please enter a number");
+        return
+    }
+return'${username} just logged in' }
+console.log(loggedinusermessage("tahreem"))
