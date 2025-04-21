58bf1054ed44923bd21f1026be23b704a783f609
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-21 09:06:49 +0000

    object.js

--- 02_basic/object.js
+++ 02_basic/object.js
@@ -20,3 +20,58 @@ console.log(myobject)
 Js.greeting=function(){
 console.log("hello")}
 console.log(Jsuser.greeting)
+Js.greeting=function(){
+    console.log('Heloo js user,$(this is .name)' );
+
+}
+console.log(Js.greeting);
+// Singleton///
+google.id="123abc";
+google.name="sammay"
+google.isLoggedIn="flase"
+//console.log(google);
+const regularuser={
+    email:"tahreemtajally@gmail.com",
+   userfullname:{fullname:{
+userfullname:{
+    fristname:"tahreem",
+    lastname:"tajalli"
+}}
+    }
+}
+console.log(regularuser.userfullname)
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+const obj3={5:"a", 6:"b"}
+//const obj4= object.assign({},obj1,obj2,obj3)
+//const obj4= {...obj1,...obj2}
+//console.log(obj4);
+const user=[
+    {
+        id:1,
+        email:"tahreemtajalli@gmail.com"
+    },
+    {
+        id:1,
+        email:"tahreemtajalli@gmail.com"
+    },
+    {
+        id:1,
+        email:"tahreemtajalli@gmail.com"
+    }
+]
+user [1].email
+console.log(google);
+//console.log(object.keys(google));
+//console.log(object.value(google));
+//console.log(object.entires(google));
+//console.log(google.hasownprperty('isLoggedIn'));
+//Data structing//
+const course = {
+    email:"tahreemtajalii@gmail.com",
+    name:tahreem,
+    courseInstructor:tajalli
+}
+const{courseInstructor}=course
+console.log(courseInstructor);
