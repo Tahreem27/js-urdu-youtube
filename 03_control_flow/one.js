724c2c01698762d280686e41bcab2cb2c571e565
Tahreem27 <152021286+Tahreem27@users.noreply.github.com>
2025-04-24 13:23:40 +0000

    one.js

---
+++ 03_control_flow/one.js
@@ -0,0 +1,35 @@
+//if
+//const isLoggedIn= true
+//if (2==="2") {
+   // console.log(executed);
+    
+//}
+// <=,>=,==,====(check the type),!=,!==(question )
+//const isLoggedIn= true
+//const temperture=41
+//if (temperture<=50) {
+   // console.log("temperture  < 50");}
+    //else{
+    //console.log("temperture  is 50");}
+    //if (scorce>100){
+      //  let power="fly"
+       // console.log(`user power is${power}`)
+   // }
+    //console.log(`user power is${power}`)
+   // const balance=1000
+   // if (balance<=500) {
+      //  console.log("balance  < 500");}
+      // elseif(balance>=900)
+     //  {
+      //  console.log("balance  is 900");}
+      //  elseif(balance>=1000)
+      // {
+        //console.log("balance  is 900");}
+    //else{
+        //console.log("less than 300");
+    //}
+const UserLoggedIn= true
+const debitcard= true
+if (UserLoggedIn && deditcard &&2==2){
+    console.log("allow the course")
+}
